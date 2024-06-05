4653c131742b1a647f44a5e1688d34ba5055a236
baowenqian2001 <Bao_Wenqian@126.com>
2024-06-05 15:53:49 +0000

    main

--- cnn/AlexNet.py
+++ cnn/AlexNet.py
@@ -2,5 +2,41 @@ import torch
 import torch.nn as nn
 
 class AlexNet(nn.Module):
-    def __init__(self, num_classer=1000):
-        super().__init__()
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),  # input[3, 224, 224]  output[96, 55, 55]
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # output[96, 27, 27]
+
+            nn.Conv2d(96, 256, kernel_size=5, padding=2),  # output[256, 27, 27]
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # output[256, 13, 13]
+
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),  # output[384, 13, 13]
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(384, 384, kernel_size=3, padding=1),  # output[384, 13, 13]
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),  # output[256, 13, 13]
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # output[256, 6, 6]
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 1000),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(1000, num_classes),
+        )
+
+def forward(self, x):
+    x = self.features(x) # 256 * [6 * 6]
+    x = torch.flatten(x, start_dim=1) # shape 1-> 合并
+    x = self.classifier(x)
+    return x
